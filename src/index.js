49326b7bbeaa9b5ce43463ac5429df6d951c85cf
Dmitry Shur <dimashur@gmail.com>
2018-03-09 15:55:11 -0500

    added singleton pattern

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { SingletonTester, Singleton } from './patterns/singleton/singleton';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const singletonTest = SingletonTester.getInstance({
+  pointX: 5,
+});
+
+const sin = new Singleton(10, 20);
+sin.getInstance;
+
+console.log(singletonTest.pointX);
 registerServiceWorker();
